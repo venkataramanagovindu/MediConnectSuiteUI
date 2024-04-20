0cd8360889f182cfd157e76e5babb23c54c74031
jasweerbannu <bannubheem76@gmail.com>
2024-04-20 11:14:44 -0700

    SignUp and SignIn Changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <SignIn></SignIn>
+    <SignUp></SignUp>
+    
+   
+      {/* <Router>
+        <Switch>
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+          <Redirect to="/signin" />
+        </Switch>
+      </Router> */}
+    
+  
+
+
+  </div>;
 }
 
 export default App;
